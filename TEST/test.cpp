affb4812335c9d956fd2c1eaa7a4ccc37db31375
tomorrow_morning_will_be_fine <1005489594@qq.com>
2023-08-18 23:40:51 +0800

    Note

--- TEST/test.cpp
+++ TEST/test.cpp
@@ -8,8 +8,10 @@ const int& f2(int& a);
 
 int main() {
     int num1 = 1, num2 = 2;
+    const int num3 = 3;
     f1(num1) = 5;//可以通过编译，语句执行后num==5。
-    f2(num2) = 9;//无法通过编译，f2(num2)无法作为左值。
+    cout << num1 << endl;
+    // f2(num2) = 9;//无法通过编译，f2(num2)无法作为左值。
 }
 
 int& f1(int& a) {
