61799a2a162c528b79ce39d29bd1cbe43f9cb6c9
tomorrow_morning_will_be_fine <1005489594@qq.com>
2023-08-20 14:26:58 +0800

    chapter 12 3

---
+++ C++ primer plus/Chapter 12/3/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/rick/Documents/C++ practice/C++ primer plus/Chapter 12/3/bin/main"
+  "/home/rick/Documents/C++ practice/C++ primer plus/Chapter 12/3/bin/main.pdb"
+  "CMakeFiles/main.dir/stock20.cpp.o"
+  "CMakeFiles/main.dir/stock20.cpp.o.d"
+  "CMakeFiles/main.dir/usestok2.cpp.o"
+  "CMakeFiles/main.dir/usestok2.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
