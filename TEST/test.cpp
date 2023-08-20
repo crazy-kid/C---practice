7a1c8a5b46ccbb97c739992a3bcc63b874f66588
tomorrow_morning_will_be_fine <1005489594@qq.com>
2023-08-20 19:59:22 +0800

    NOTE

--- TEST/test.cpp
+++ TEST/test.cpp
@@ -7,15 +7,14 @@ using namespace std;
 int& f1(int& a);
 const int& f2(int& a);
 
-int main() {
-    int num1 = 1, num2 = 2;
-    const int num3 = 3;
-    f1(num1) = 5;//可以通过编译，语句执行后num==5。
-    cout << num1 << endl;
-    // f2(num2) = 9;//无法通过编译，f2(num2)无法作为左值。
-    char* str = "abcdefg";
-    cout << strlen(str);
+int main(int argc, char const* argv[])
+{
+    /* code */
+    return 0;
 }
+
+
+
 
 int& f1(int& a) {
     a++;
